9048f2836941d3aca8bade09ea9562dd68953f73
victormdsm <victor.de.souza.muller@gmail.com>
2024-05-06 16:38:21 -0300

    first class

---
+++ src/main/java/com/delivery/deliveryapp/entities/User.java
@@ -0,0 +1,77 @@
+package com.delivery.deliveryapp.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, phone);
+    }
+}
