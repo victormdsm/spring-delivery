4c501581fdf55bd441a2bf54857b1e88b4de13fb
victormdsm <victor.de.souza.muller@gmail.com>
2024-05-06 16:18:57 -0300

    full

---
+++ src/main/java/com/delivery/deliveryapp/DeliveryappApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.deliveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryappApplication.class, args);
+	}
+
+}
